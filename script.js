d40a2a8be7448db11126a69dcdc42f2d41c168ad
Mack Maier <hmaiermack@gmail.com>
2019-12-04 19:37:29 -0800

    testing code w/ nick

---
+++ script.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFpZXJtYWNrIiwiYSI6ImNrM3JvODA1ODA4Z2IzaHVoams2Zm41YTEifQ.Cb3O-AwARlOakYrbV9erjQ';
+
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/maiermack/ck3rxyctv0ekb1cou5g6cff9u'
+});
